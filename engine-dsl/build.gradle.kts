7df0d2a7762876e6e46d9679d8d698cd7c15037b
a.m.kirzhaev <a.m.kirzhaev@tinkoff.ru>
2019-12-08 02:21:27 +0300

    fix build

--- engine-dsl/build.gradle.kts
+++ engine-dsl/build.gradle.kts
@@ -2,6 +2,9 @@ plugins {
     kotlin("jvm")
 }
 
+group = "kt.team"
+version = "0.0"
+
 dependencies {
     compile(kotlin("stdlib"))
 }
@@ -10,7 +13,7 @@ val fatJar = task("fatJar", type = Jar::class) {
     baseName = "${project.name}-fat"
     manifest {
         attributes["Implementation-Version"] = version
-        attributes["Main-Class"] = "kt.team.api.NSApp"
+        attributes["Main-Class"] = "kt.team.dsl.function"
     }
     from(configurations.runtimeClasspath.get().map({ if (it.isDirectory) it else zipTree(it) }))
     with(tasks.jar.get() as CopySpec)
