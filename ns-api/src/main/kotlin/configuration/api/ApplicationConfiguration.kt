56b5257726bd1f43e7a75529060c916aa9bef699
lanneta <chernisheva.lada@gmail.com>
2019-12-08 09:57:58 +0300

    added last front rest method

--- ns-api/src/main/kotlin/configuration/api/ApplicationConfiguration.kt
+++ ns-api/src/main/kotlin/configuration/api/ApplicationConfiguration.kt
@@ -2,7 +2,6 @@ package configuration.api
 
 import io.ktor.application.Application
 import io.ktor.routing.routing
-import org.koin.ktor.ext.get
 
 fun Application.configureApi() {
     routing {
--- ns-api/src/main/kotlin/persist/customtypes/TypeReference.kt
+++ ns-api/src/main/kotlin/persist/customtypes/TypeReference.kt
@@ -1,5 +1,3 @@
 package persist.customtypes
 
-import com.fasterxml.jackson.core.type.TypeReference
-
 //inline fun <reified T: Any> typeRef() = object: TypeReference<T>() {}::class.java
